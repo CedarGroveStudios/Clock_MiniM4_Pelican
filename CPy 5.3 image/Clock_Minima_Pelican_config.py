eb6e5009d1d256624ba211997e7bdd30e7effc7f
Cedar Grove Maker Studios <jan@goolsbey.net>
2020-07-25 18:08:22 -0700

    update CPy 5.3 image folder

--- CPy 5.3 image/Clock_Minima_Pelican_config.py
+++ CPy 5.3 image/Clock_Minima_Pelican_config.py
@@ -5,3 +5,4 @@ clock_24_hour  = False      # 24-hour clock = True; 12-hour AM/PM = False
 clock_auto_dst = True       # Automatic US DST = True
 clock_sound    = False      # Sound effects active = True
 clock_tick     = True       # One-second tick sound effect
+clock_bright   = 1.0        # Display brightness; 0 (low) to 1.0 (high)
